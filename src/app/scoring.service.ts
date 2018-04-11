9a75520be254db7a0df90034f1c98d3e5bb15af4
Reed Lawrence <reed.g.lawrence@gmail.com>
2018-04-11 16:35:41 -0500

    commit

--- src/app/scoring.service.ts
+++ src/app/scoring.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Scoring } from './scoring';
+import { Qb } from './qb';
+import { QbgameLog } from './qbgame-log';
 
 @Injectable()
 export class ScoringService {
 
   constructor() { }
+
+  GetScore(scoringRule: Scoring, game: any): number {
+    let score = 0;
+    if (game instanceof QbgameLog) {
+      score += (game.PaYds * scoringRule.PaYa) / scoringRule.PaYaInterval;
+    }
+
+    return score;
+  }
 
 }
